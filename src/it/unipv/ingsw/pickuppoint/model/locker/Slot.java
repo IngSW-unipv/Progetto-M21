f216df6e14ddee33951bd61512b72a293be3fbbc
Francesco <francesco.marzullo01@universitadipavia.it>
2021-05-16 10:33:56 +0200

    Add Slot.java

--- src/it/unipv/ingsw/pickuppoint/model/locker/Slot.java
+++ src/it/unipv/ingsw/pickuppoint/model/locker/Slot.java
@@ -1,5 +1,24 @@
 package it.unipv.ingsw.pickuppoint.model.locker;
 
 public class Slot {
+	private int height;
+	private int width;
+	private int length;
+	private SlotSize size;
 
+	public Slot(SlotSize size) {
+		this.size = size;
+		initSize();
+	}
+
+	private void initSize() {
+		this.height = size.getHeight();
+		this.width = size.getWidth();
+		this.length = size.getLength();
+	}
+
+	@Override
+	public String toString() {
+		return "Slot [altezza=" + height + ", larghezza=" + width + ", lunghezza=" + length + ", size=" + size + "]";
+	}
 }
