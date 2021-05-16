a53b4f947ca84f1223124a8a3e8ff903781d2f7d
Francesco <francesco.marzullo01@universitadipavia.it>
2021-05-16 12:19:55 +0200

    add method for slot availability by size

--- src/it/unipv/ingsw/pickuppoint/model/locker/Locker.java
+++ src/it/unipv/ingsw/pickuppoint/model/locker/Locker.java
@@ -1,5 +1,50 @@
 package it.unipv.ingsw.pickuppoint.model.locker;
 
-public class Locker {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
+public class Locker {
+	private int lockerId;
+	private LockerAddress lockerAddress;
+	private Map<Integer, Slot> lockerNumberToSlot;
+
+	public Locker(int lockerId, String city, String address, int postalCode) {
+		this.lockerId = lockerId;
+		lockerAddress = new LockerAddress(city, address, postalCode);
+		lockerNumberToSlot = new HashMap<Integer, Slot>();
+	}
+	
+	public boolean isAvailable(SlotSize slotSize) {
+		for (Entry<Integer, Slot> me : lockerNumberToSlot.entrySet()) {
+			Slot slot = me.getValue();
+			if (slot.isEmpty() && slot.getSize() == slotSize) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+//	/**
+//	 * @param required slot size
+//	 * @return slot key
+//	 */
+//	public Integer getSlotId(SlotSize slotSize) {
+//		for (Entry<Integer, Slot> me : lockerNumberToSlot.entrySet()) {
+//			Slot slot = me.getValue();
+//			
+//			if (slot.isEmpty() && slot.getSize() == slotSize) {
+//				return me.getKey();
+//			}
+//		}
+//		return -1;
+//	}
+
+	public int getLockerNumber() {
+		return lockerId;
+	}
+
+	public LockerAddress getLockerAddress() {
+		return lockerAddress;
+	}
 }
