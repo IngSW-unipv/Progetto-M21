423c9cd330c4b3dc3162028e1f5c3a1d68f2e270
Francesco <francesco.marzullo01@universitadipavia.it>
2021-05-17 22:49:37 +0200

    Added method for creating slots

--- src/it/unipv/ingsw/pickuppoint/model/locker/Locker.java
+++ src/it/unipv/ingsw/pickuppoint/model/locker/Locker.java
@@ -7,6 +7,7 @@ import java.util.Map.Entry;
 import it.unipv.ingsw.pickuppoint.model.delivery.Product;
 
 public class Locker {
+	private static final int SLOTNUMBER = 15;
 	private int lockerId;
 	private LockerAddress lockerAddress;
 	private Map<Integer, Slot> lockerNumberToSlot;
@@ -22,6 +23,18 @@ public class Locker {
 		this.lockerId = lockerId;
 		lockerAddress = new LockerAddress(city, address, postalCode);
 		lockerNumberToSlot = new HashMap<Integer, Slot>();
+		addSlots();
+	}
+
+	private void addSlots() {
+		for (int i = 0; i < SLOTNUMBER; i++) {
+			if (i <= Math.round(SLOTNUMBER / 3))
+				lockerNumberToSlot.put(i, new Slot(SlotSize.BIG));
+			else if (i < Math.round(SLOTNUMBER / 3 * 2))
+				lockerNumberToSlot.put(i, new Slot(SlotSize.MEDIUM));
+			else
+				lockerNumberToSlot.put(i, new Slot(SlotSize.SMALL));
+		}
 	}
 
 	/**
