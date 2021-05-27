353fa27ed8d881855d4ce2d2d4d0d533f77036c8
Francesco <francesco.marzullo01@universitadipavia.it>
2021-05-27 13:04:15 +0200

    Aggiunto metodo per la ricerca di user in base al ruolo

--- src/main/java/it/unipv/ingsw/pickuppoint/data/UserRepo.java
+++ src/main/java/it/unipv/ingsw/pickuppoint/data/UserRepo.java
@@ -1,7 +1,5 @@
 package it.unipv.ingsw.pickuppoint.data;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,13 +8,5 @@ import it.unipv.ingsw.pickuppoint.model.User;
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
 	User findByEmail(String email);
-
-	/**
-	 * Questo metodo cerca l'utenti per ruolo
-	 * 
-	 * @param name del ruolo
-	 * @return lista di user
-	 */
-	List<User> findByRole_name(String name);
 
 }
