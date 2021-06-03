aa9a997b54a6feb868fb87a057d114e76b9bc1ce
Francesco <francesco.marzullo01@universitadipavia.it>
2021-06-03 21:05:38 +0200

    edit #2

--- src/main/java/it/unipv/ingsw/pickuppoint/controller/CustomerController.java
+++ src/main/java/it/unipv/ingsw/pickuppoint/controller/CustomerController.java
@@ -1,66 +1,77 @@
 package it.unipv.ingsw.pickuppoint.controller;
 
-import javax.validation.Valid;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.unipv.ingsw.pickuppoint.model.User;
-import it.unipv.ingsw.pickuppoint.service.CustomerService;
-import it.unipv.ingsw.pickuppoint.service.exception.CustomerAlreadyExistException;
+import it.unipv.ingsw.pickuppoint.service.HubService;
+import it.unipv.ingsw.pickuppoint.service.OrderDetailsService;
+import it.unipv.ingsw.pickuppoint.service.UserService;
 
 @Controller
 public class CustomerController {
 	@Autowired
-	private CustomerService customerService;
+	UserService userService;
+	@Autowired
+	OrderDetailsService orderDetailsService;
+	@Autowired
+	HubService hubService;
+
 
 	/**
 	 * Questo metodo viene invocato quando il client effettua una richiesta GET a
-	 * /register.
-	 * Viene istanziato un nuovo customer, viene aggiunto al modello che
-	 * a sua volta viene inoltrato al client per essere manipolato.
+	 * /viewCustomerOrders. Recupera e visualizza gli ordini del Customer.
 	 * 
 	 * @param model è un contenitore di attributi che viene inoltrato al client per
 	 *              essere visualizzato o manipolato
-	 * @return la pagina html di registrazione
+	 * @return la pagina html della vista degli ordini
 	 */
-	@RequestMapping("/register")
-	public String register(Model model) {
-		User customer = new User();
-		model.addAttribute("user", customer);
-		return "registration";
+	@RequestMapping("/viewCustomerOrders")
+	public String viewCustomerOrders(Model model) {
+		User user = userService.getAuthenticatedUser();
+
+		// dovrebbe essere user.getOrder
+		model.addAttribute("listOrders", orderDetailsService.getCustomerOrders(user.getUserId()));
+		return "viewOrders";
 	}
+	
 
 	/**
-	 * Questo metodo viene invocato quando il client effettua una richiesta POST a
-	 * /register. Il client inoltra al server:
+	 * Questo metodo viene invocato quando il client effettua una richiesta GET a
+	 * /add. Aggiunge un prodotto nella lista dei prodotti del Customer tramite
+	 * inserimento del tracking code; Viene aggiunto il riferimento di chiave
+	 * esterna (CustomerID) all'interno dell'entità genitore OrderDetails
 	 * 
-	 * @param customer      popolato dai sui attributi attraverso input html
-	 * @param bindingResult per la verifica di errori di registrazione
-	 * @param model         si riferisce all'istanza del Customer, utile nel caso in
-	 *                      cui ci fossero degli errori nella registrazione per
-	 *                      inoltrare la stessa istanza (ed evitare di crearne
-	 *                      un'altra) alla pagina di registrazione
-	 * @return In caso di errori ritorna la pagina di registrazione, altrimenti
-	 *         ritorna un reindirizzamento alla pagina root 
+	 * @param tracking code dell'ordine
+	 * @return reindirizzamento alla pagina html della vista degli ordini
 	 */
-	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String userRegistration(@Valid User customer, BindingResult bindingResult, Model model) {
-		if (bindingResult.hasErrors()) {
-			model.addAttribute("user", customer);
-			return "registration";
-		}
-		try {
-			customerService.register(customer);
-		} catch (CustomerAlreadyExistException e) {
-			bindingResult.rejectValue("email", "user.email", "An account already exists for this email");
-			model.addAttribute("user", customer);
-			return "registration";
-		}
-		return "redirect:" + "/"; // Ritorna alla schermata di login
+	@Transactional
+	@RequestMapping(value = "/add")
+	public String addOrder(@RequestParam(name = "tracking") int tracking) {
+		hubService.addOrder(tracking);
+		return "redirect:" + "/viewCustomerOrders";
+	}
+	
+
+	/**
+	 * Questo metodo viene invocato quando il client effettua una richiesta GET a
+	 * /withdraw/{id}; Permette al Customer di ritirare l'ordine; Viene settato
+	 * DeliveryStatus = WITHDRAW e WithdrawalDate
+	 * 
+	 * @param id ordine da ritirare
+	 * @return reindirizzamento alla pagina per il recupero e la visualizzazione
+	 *         degli ordini del Customer attraverso una richesta GET da parte del
+	 *         client
+	 */
+	@RequestMapping("/withdraw/{id}")
+	public String showEditProductForm(@PathVariable(name = "id") Long id) {
+		hubService.withdraw(id);
+		return "redirect:" + "/viewCustomerOrders";
 	}
 }
--- src/main/java/it/unipv/ingsw/pickuppoint/controller/UserController.java
+++ src/main/java/it/unipv/ingsw/pickuppoint/controller/UserController.java
@@ -1,14 +1,102 @@
 package it.unipv.ingsw.pickuppoint.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.unipv.ingsw.pickuppoint.model.User;
 import it.unipv.ingsw.pickuppoint.service.UserService;
+import it.unipv.ingsw.pickuppoint.service.exception.CustomerAlreadyExistException;
 
 @Controller
 public class UserController {
-
 	@Autowired
 	UserService userService;
+
+	/**
+	 * Questo metodo viene invocato quando il client effettua una richiesta GET alla
+	 * root del sito
+	 * 
+	 * @return home page
+	 */
+	@RequestMapping("/")
+	public String homePage() {
+		return "index";
+	}
+
+	/**
+	 * Questo metodo viene invocato quando il client effettua una richiesta GET a
+	 * /login
+	 * 
+	 * @return pagina di login
+	 */
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	/**
+	 * Questo metodo viene invocato quando il client effettua una richiesta GET a
+	 * /profile, subito dopo il login
+	 * 
+	 * @return pagina html profilo
+	 */
+	@RequestMapping("/profile")
+	public String viewHomePage() {
+		return "profile";
+	}
+	
+
+	/**
+	 * Questo metodo viene invocato quando il client effettua una richiesta GET a
+	 * /register.
+	 * Viene istanziato un nuovo customer, viene aggiunto al modello che
+	 * a sua volta viene inoltrato al client per essere manipolato.
+	 * 
+	 * @param model è un contenitore di attributi che viene inoltrato al client per
+	 *              essere visualizzato o manipolato
+	 * @return la pagina html di registrazione
+	 */
+	@RequestMapping("/register")
+	public String register(Model model) {
+		User customer = new User();
+		model.addAttribute("user", customer);
+		return "registration";
+	}
+
+	/**
+	 * Questo metodo viene invocato quando il client effettua una richiesta POST a
+	 * /register. Il client inoltra al server:
+	 * 
+	 * @param customer      popolato dai sui attributi attraverso input html
+	 * @param bindingResult per la verifica di errori di registrazione
+	 * @param model         si riferisce all'istanza del Customer, utile nel caso in
+	 *                      cui ci fossero degli errori nella registrazione per
+	 *                      inoltrare la stessa istanza (ed evitare di crearne
+	 *                      un'altra) alla pagina di registrazione
+	 * @return In caso di errori ritorna la pagina di registrazione, altrimenti
+	 *         ritorna un reindirizzamento alla pagina root 
+	 */
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String userRegistration(@Valid User customer, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("user", customer);
+			return "registration";
+		}
+		try {
+			userService.register(customer);
+		} catch (CustomerAlreadyExistException e) {
+			bindingResult.rejectValue("email", "user.email", "An account already exists for this email");
+			model.addAttribute("user", customer);
+			return "registration";
+		}
+		return "redirect:" + "/"; // Ritorna alla schermata di login
+	}
+	
 
 }
