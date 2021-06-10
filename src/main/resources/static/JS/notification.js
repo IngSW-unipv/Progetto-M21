a6a5f4069cd65a32cf0c06f5f40039f35a111b09
Francesco <francesco.marzullo01@universitadipavia.it>
2021-06-10 12:50:44 +0200

    Update: utilizzo API js.cookies

--- src/main/resources/static/JS/notification.js
+++ src/main/resources/static/JS/notification.js
@@ -1,14 +1,13 @@
-var arrayCourier = [];
-var arrayCustomer = [];
-
+var arrayCourier = new Array();
+var arrayCustomer = new Array();
 
 function notify(orderId, role) {
 	if (Notification.permiss == 'granted') {
-		roleNotification();
+		roleNotification(orderId, role)
 	} else if (Notification.permission !== "denied") {
 		Notification.requestPermission().then(permission => {
 			if (permission == "granted") {
-				roleNotification(orderId, role);
+				roleNotification(orderId, role)
 			}
 		});
 	}
@@ -20,71 +19,49 @@ function roleNotification(orderId, role) {
 	} else if (role == 'COURIER') {
 		courierNotification(orderId);
 	}
-
 }
 
 function customerNotification(orderId) {
-	var orders = getCookie('customer');
+	var orders = Cookies.get('customer');
 
-	if (orders.isEmpty) {
-		createCookie('customer', arrayCustomer);
-	}
+	console.log("orders " + orders);
+	console.log("ID " + orderId);
+	console.log("arrayCustomer " + arrayCustomer);
 
-	if (!orders.includes(orderId)) {
+	if (orders == null || !orders.includes(orderId)) {
 		new Notification("Notifica consegna avvenuta", {
 			body: "ordine " + orderId + " CONSEGNATO",
 			icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/delivered-1681114-1427946.png"
 		});
 		arrayCustomer.push(orderId);
-		createCookie('customer', arrayCustomer);
+
+		//ERRORE: 
+		// La prima invocazione del metodo funziona: vengono pushati gli orderId nell'array e viene settato 
+		// l'array dentro il cookie
+		
+		// PROBLEMA: dopo l'aggiornamento della pagina arrayCustomer viene inizializzato, di conseguenza
+		// vengono persi tutti gli elementi quindi il cookie verrà settato solo con  l'orderId che ha invocato il metodo
+		
+		// SOLUZIONE: trovare un modo per aggiungere al cookie elemento per elemento
+		// for con indice che inizia da cookie.size() e termina cookie.size()+1
+		
+		Cookies.set('customer', JSON.stringify(arrayCustomer));
+		console.log("#arrayCustomer " + arrayCustomer);
 	}
 }
-
 function courierNotification(orderId) {
-	var orders = getCookie('courier');
+	var orders = Cookies.get('courier');
 
-	if (orders.isEmpty) {
-		createCookie('courier', arrayCourier);
-	}
+	console.log("orders " + orders);
+	console.log("ID " + orderId);
+	console.log("arrayCustomer " + arrayCustomer);
 
-	if (!orders.includes(orderId)) {
+	if (orders == null || !orders.includes(orderId)) {
 		new Notification("Notifica ritiro avvenuto", {
 			body: "ordine " + orderId + " RITIRATO",
 			icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Light_green_check.svg/480px-Light_green_check.svg.png"
 		});
 		arrayCourier.push(orderId);
-		createCookie('courier', arrayCourier);
+		Cookies.set('courier', JSON.stringify(arrayCourier));
 	}
 }
-
-function createCookie(name, value) {
-
-	var cookie = [
-		name,
-		'=',
-		JSON.stringify(value)
-
-	].join('');
-	document.cookie = cookie;
-
-}
-
-function getCookie(name) {
-	var a = [];
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ')
-			c = c.substring(1, c.length);
-			
-		if (c.indexOf(nameEQ) === 0) {
-			return JSON.parse(
-				c.substring(nameEQ.length, c.length)
-			);
-		}
-	}
-	return a;
-}
-
