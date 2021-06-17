b14161f63176b9dfc0231aee87c49b3e54195641
rosa-mn <rosamaria.nucera02@universitadipavia.it>
2021-06-17 13:11:51 +0200

    Aggiunta pagina editUser e corrispettivo backend

--- src/main/java/it/unipv/ingsw/pickuppoint/controller/AdministratorController.java
+++ src/main/java/it/unipv/ingsw/pickuppoint/controller/AdministratorController.java
@@ -1,35 +1,78 @@
 package it.unipv.ingsw.pickuppoint.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.unipv.ingsw.pickuppoint.data.UserRepo;
+import it.unipv.ingsw.pickuppoint.model.User;
 import it.unipv.ingsw.pickuppoint.service.UserService;
 
 @Controller
 public class AdministratorController {
-	
+
 	@Autowired
 	UserService userService;
-	
+	@Autowired
+	UserRepo userRepo;
+
+	/**
+	 * Questo metodo viene invocato quando il client invia una richiesta GET a
+	 * /userList dalla pagina profile.
+	 * 
+	 * @param model contenitore di attributi dove vengono inseriti gli utenti dal
+	 *              database
+	 * @return la pagina userList
+	 */
+
 	@RequestMapping("/UserList")
 	public String viewHomePage(Model model) {
 		userService.findAllUsers(model);
 		return "userList";
 	}
-	
-	@RequestMapping("/editUser/{id}")
+
+	/**
+	 * Questo metodo viene invocato quando il client invia una richiesta GET a
+	 * /editUser dalla pagina userList.
+	 * 
+	 * @param id    è l'id dell'utente da modificare
+	 * @param model è il contenitore di attributi che viene inoltrato al client per
+	 *              inserire i dati del nuovo utente
+	 * @return la pagina del form di modifica utente
+	 */
+	@RequestMapping(value = "/editUser/{id}")
 	public String showEditUserForm(@PathVariable(name = "id") Long id, Model model) {
-		userService.editUser(model, id);
-		return "redirect:" + "/editUser";
+		model.addAttribute(userService.getUser(id));
+		return "/editUser";
 	}
 
-//	@RequestMapping("/addUser")
-//	public String addUser(Model model) {
-//		//userService.editUser(model);
-//		return "redirect:" + "/editUser";
-//	}
-	
+	/**
+	 * Questo metodo viene invocato quando il client invia una richiesta POST a
+	 * /save dalla pagina editUser.
+	 * 
+	 * @param id            è l'id dell'utente modificato
+	 * @param user          è l'utente con le nuove modifiche
+	 * @param bindingResult per la verifica di errori di compilazione del form
+	 * @param model         si riferisce all'istanza di user, serve in caso di
+	 *                      errori nella compilazione.
+	 * @return	la pagina stessa editUser in caso di problemi, o userList in caso
+	 *			di successo.
+	 */
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveUser(@Valid  @ModelAttribute User user, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("user", user);
+			return "editUser";
+		}
+		userService.saveUser(user);
+		return "redirect:" + "/UserList";
+	}
 }
--- src/main/java/it/unipv/ingsw/pickuppoint/model/User.java
+++ src/main/java/it/unipv/ingsw/pickuppoint/model/User.java
@@ -70,6 +70,10 @@ public class User {
 	public Long getUserId() {
 		return userId;
 	}
+	
+	public void setUserId(Long userId) {
+		this.userId=userId;
+	}
 
 	public boolean isEnabled() {
 		return Enabled;
