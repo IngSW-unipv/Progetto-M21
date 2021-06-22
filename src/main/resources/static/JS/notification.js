ffcce34a89d02af9a3aeba44e883135100f497e1
Francesco <francesco.marzullo01@universitadipavia.it>
2021-06-22 21:47:46 +0200

    Risolto bug notifica

--- src/main/resources/static/JS/notification.js
+++ src/main/resources/static/JS/notification.js
@@ -1,49 +1,47 @@
-function notify() {
-	if (Notification.permiss == 'granted') {
-		return;
+function notify(callback) {
+	if (Notification.permiss == "granted") {
+		callback();
 	} else if (Notification.permission !== "denied") {
-		Notification.requestPermission().then(permission => {
+		Notification.requestPermission().then((permission) => {
 			if (permission == "granted") {
-				return;
+				callback();
 			}
 		});
 	}
 }
 
-
 function customerNotification(orderId) {
-	notify();
-	var orders = Cookies.get('customer');
+	notify(() => {
+		var orders = Cookies.get("customer");
 
-	console.log("orders " + orders);
-	console.log("ID " + orderId + " ####");
+		console.log("orders " + orders);
+		console.log("ID " + orderId + " ####");
 
-	if (orders == null || !orders.includes(orderId)) {
-		new Notification("Notifica consegna avvenuta", {
-			body: "ordine " + orderId + " CONSEGNATO",
-			icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/delivered-1681114-1427946.png"
-		});
-		setCookie("customer", orderId);
-	}
+		if (orders == null || !orders.includes(orderId)) {
+			new Notification("Notifica consegna avvenuta", {
+				body: "ordine " + orderId + " CONSEGNATO",
+				icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/delivered-1681114-1427946.png",
+			});
+			setCookie("customer", orderId);
+		}
+	});
 }
-
 
 function courierNotification(orderId) {
-	notify();
-	var orders = Cookies.get('courier');
-	console.log("orders " + orders);
-	console.log("ID " + orderId);
+	notify(() => {
+		var orders = Cookies.get("courier");
+		console.log("orders " + orders);
+		console.log("ID " + orderId);
 
-	if (orders == null || !orders.includes(orderId)) {
-		new Notification("Notifica ritiro avvenuto", {
-			body: "ordine " + orderId + " RITIRATO",
-			icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Light_green_check.svg/480px-Light_green_check.svg.png"
-		});
-
-		setCookie("courier", orderId);
-	}
+		if (orders == null || !orders.includes(orderId)) {
+			new Notification("Notifica ritiro avvenuto", {
+				body: "ordine " + orderId + " RITIRATO",
+				icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Light_green_check.svg/480px-Light_green_check.svg.png",
+			});
+			setCookie("courier", orderId);
+		}
+	});
 }
-
 
 function setCookie(name, value) {
 	var x = Cookies.get(name);
